bbb77e90c50041fa777b356e720b1d512882be00
홍덕화(Deokhwa Hong) 연구원 HD현대인프라코어 <deokhwa.hong@hd.com>
2024-05-16 15:57:35 +0900

    24.05.16 second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,6 +19,27 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: IconButton(
+              icon: Image.asset('images/dice1.png'),
+              onPressed: () {
+                print('Left button got pressed.');
+              },
+            ),
+          ),
+          Expanded(
+            child: IconButton(
+              icon: Image.asset('images/dice2.png'),
+              onPressed: () {
+                print('Right button got pressed.');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

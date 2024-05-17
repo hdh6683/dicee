2966bdf24ac4d735d73eb589dbe47bfbd65d98c0
홍덕화(Deokhwa Hong) 연구원 HD현대인프라코어 <deokhwa.hong@hd.com>
2024-05-17 11:05:39 +0900

    Completed Dicee Project commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,22 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -24,17 +40,20 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: IconButton(
-              icon: Image.asset('images/dice1.png'),
+              icon: Image.asset('images/dice$leftDiceNumber.png'),
               onPressed: () {
-                print('Left button got pressed.');
+                setState(() {});
+                changeDiceFace();
               },
             ),
           ),
           Expanded(
             child: IconButton(
-              icon: Image.asset('images/dice2.png'),
+              icon: Image.asset('images/dice$rightDiceNumber.png'),
               onPressed: () {
-                print('Right button got pressed.');
+                setState(() {
+                  changeDiceFace();
+                });
               },
             ),
           ),
